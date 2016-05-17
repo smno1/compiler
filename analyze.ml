91efce3dfaf3b68d2c5afce5c0eb98a5868da552
sheen <ysmsmno1@gmail.com>
2016-05-17 16:04:38 +1000

    current field name

--- analyze.ml
+++ analyze.ml
@@ -1,22 +1,26 @@
 open Symbol_table
+open Bean_ast
 
-let alyz_fielddef suptype (iden,fieldtype)=
-  match fieldtype with
+let proc_slot_count=ref 0;
+
+let rec alyz_fielddef_list suptype flist =
+    List.iter (alyz_fielddef suptype) flist
+and alyz_fielddef suptype (iden,fieldtype)=
+    match fieldtype with
         Bool-> Symbol_table.add_fielddef {fieldname=iden;  field_typespec="bool"; belong_type=suptype; 
                 field_size=1;sub_field=false }
       | Int -> Symbol_table.add_fielddef {fieldname=iden;  field_typespec="int"; belong_type=suptype; 
                 field_size=1; sub_field=false}
       | Flist(flst)-> alyz_fielddef_list iden flst;
                       let typesize=(calc_size_type iden) in
-                      Symbol_table.add_fielddef{fieldname=iden; field_typespec=iden; type_size=typesize; sub_field=true} 
+                      Symbol_table.add_fielddef{fieldname=iden; field_typespec=iden; belong_type=suptype; 
+                      field_size=typesize; sub_field=true} 
       | Id(id)-> let origin_type=Symbol_table.look_up_origin_type id in
                     Symbol_table.add_fielddef {fieldname=iden;  
                     field_typespec=origin_type;
                     belong_type=suptype; field_size=(find_typedef origin_type).type_size;
                     sub_field=(origin_type != "int" && origin_type != "bool") }
-and alyz_fielddef_list suptype flist =
-  List.map (alyz_fielddef suptype) flist;
-  
+
 
 let alyz_typedef (typespec,ident)=
     match typespec with
@@ -32,7 +36,61 @@ let alyz_typedef (typespec,ident)=
                     type_size=(find_typedef origin_type).type_size; 
                     sub_type=(origin_type != "int" && origin_type != "bool") }
 
+
+
+let alyze_parameter supproc (passspec,typespec,ident)=
+    match passspec with
+        (* if it is a ref record store all the sub filed variable as ref? *)
+        Ref ->  match typespec with
+                    Bool-> Symbol_table.add_symbol {identifier=ident; slot=!proc_slot_count; sym_typespec="not_found"; 
+                            scope=supproc; sym_size=0; pass_by_ref=ref_flag}
+                  | Int -> ()
+                  | Flist(flst)-> ()
+                  | Id(id)-> ()  
+      | Val ->  false
+    (* ref doesn not need slot? *)
+    (* TO DO!! type value check: using init part, not assign yet  *)
+    if ref_flag then 
+      Symbol_table.add_symbol {identifier=ident; slot=!proc_slot_count; sym_typespec=""; 
+                  scope=supproc; sym_size=0; pass_by_ref=true}
+                proc_slot_count := !proc_slot_count+1 
+    else 
+      match typespec with
+          Bool-> Symbol_table.add_symbol {identifier=ident; slot=!proc_slot_count; sym_typespec="not_found"; 
+                  scope=supproc; sym_size=0; pass_by_ref=ref_flag}
+        | Int -> ()
+        | Flist(flst)-> ()
+        | Id(id)-> ()
+      (* haven't consider the parameter type like {int a} *)
+      (* let symbol_type = look_up_origin_type *)
+      Symbol_table.add_symbol {identifier=ident; slot=!proc_slot_count; sym_typespec="not_found"; 
+          scope=supproc; sym_size=0; pass_by_ref=ref_flag}
+    
+
+let alyz_procheader procheader=
+    List.iter (alyze_parameter procheader.procname) procheader.parameters;
+        
+let alyz_proc (procheader, procbody) =
+    proc_slot_count := ;
+    alyz_procheader procheader;
+    alyz_procbody procbody;
+    let procname=procheader.procname in
+    Symbol_table.add_proc {procname=procname; proc_size=(Symbol_table.calc_size_proc procname)}
+
 let alyz_program program = 
     Symbol_table.init();
-    List.map alyz_typedef program.typedefs
-    (* List.map cg_proc program.procs; *)
+    List.iter alyz_typedef program.typedefs;
+    List.iter alyz_proc program.procs
+
+let show_table () =
+    print_string "======type table=======\n";
+    List.iter (fun x -> print_string (x.typename^" ")) Symbol_table.typedef_table.typedef_list;
+    print_string "\n======fielsd table=======\n";
+    List.iter (fun x -> print_string (x.fieldname^" ")) Symbol_table.fielddef_table.fielddef_list;
+    ()
+
+
+
+
+
+
--- symbol_table.ml
+++ symbol_table.ml
@@ -1,8 +1,8 @@
 (* Define the types for each symbol table element *)
-type symbol={identifier:string; slot:string; sym_typespec:string; scope:string; init:bool; sym_size:int }
+type symbol={identifier:string; slot:string; sym_typespec:string; scope:string; sym_size:int; pass_by_ref:bool; super_symbol:bool }
 type typedef={typename:string;  typespec:string; type_size:int; sub_type:bool}
 type fielddef={fieldname:string;  field_typespec:string; belong_type:string; field_size:int; sub_field:bool }
-type proc={procname:string;  proc_typespec:string; proc_size:int }
+type proc={procname:string; proc_size:int }
 
 
 (* Define the 4 types of symbol tables *)
@@ -14,10 +14,10 @@ type proc_stack={mutable proc_list : proc list}
 
 (* default elements *)
 let symbol_not_found={identifier="not_found_404";slot="not_found";sym_typespec="not_found"; 
-        scope="not_found";init=false;sym_size=0}
+        scope="not_found";sym_size=0;pass_by_ref=false}
 let typedef_not_found={typename="not_found_404";  typespec="not_found"; type_size=0; sub_type=false }
 let fielddef_not_found={fieldname="not_found_404";  field_typespec="not_found"; belong_type="not_found";field_size=0;sub_field=false }
-let proc_not_found={procname="not_found_404";  proc_typespec="not_found"; proc_size=0 }
+let proc_not_found={procname="not_found_404"; proc_size=0 }
 
 
 let symbol_table= {symbol_list=[]}
@@ -27,13 +27,13 @@ let proc_table= {proc_list=[]}
 
 (* all the `find element` functions *)
 let find_symbol id scope =
-    try (List.find (fun s->s.identifier=id; s.scope=scope) symbol_table.symbol_list) with Not_found -> symbol_not_found
+    try (List.find (fun s->s.identifier=id && s.scope=scope) symbol_table.symbol_list) with Not_found -> symbol_not_found
 
 let find_typedef id =
     try (List.find (fun s->s.typename=id) typedef_table.typedef_list) with Not_found -> typedef_not_found
 
 let find_fielddef id belong_type=
-    try (List.find (fun s->s.fieldname=id; s.belong_type=belong_type) fielddef_table.fielddef_list) with Not_found -> fielddef_not_found
+    try (List.find (fun s->s.fieldname=id && s.belong_type=belong_type) fielddef_table.fielddef_list) with Not_found -> fielddef_not_found
 
 let find_proc id =
     try (List.find (fun s->s.procname=id) proc_table.proc_list) with Not_found -> proc_not_found
@@ -41,7 +41,7 @@ let find_proc id =
 
 (* all the `add element` functions *)
 let add_symbol x =
-    if not (List.exists (fun s->s.identifier=x.identifier; s.scope=x.scope) symbol_table.symbol_list) then
+    if not (List.exists (fun s->s.identifier=x.identifier && s.scope=x.scope) symbol_table.symbol_list) then
         symbol_table.symbol_list <- x::symbol_table.symbol_list
 
 let add_typedef x =
@@ -49,7 +49,7 @@ let add_typedef x =
         typedef_table.typedef_list <- x::typedef_table.typedef_list
 
 let add_fielddef x =
-    if not (List.exists (fun s->s.fieldname=x.fieldname; s.belong_type=x.belong_type) fielddef_table.fielddef_list) then
+    if not (List.exists (fun s->s.fieldname=x.fieldname && s.belong_type=x.belong_type) fielddef_table.fielddef_list) then
         fielddef_table.fielddef_list <- x::fielddef_table.fielddef_list
 
 let add_proc x =
@@ -58,8 +58,8 @@ let add_proc x =
 
 (* init primitive types *)
 let init_d () = 
-    let int_instance = { typename="int"; typespec="int"; type_size=1 } in 
-    let bool_instance = { typename="bool"; typespec="bool"; type_size=1 } in
+    let int_instance = { typename="int"; typespec="int"; type_size=1; sub_type=false } in 
+    let bool_instance = { typename="bool"; typespec="bool"; type_size=1; sub_type=false } in
     add_typedef int_instance;
     add_typedef bool_instance
 
